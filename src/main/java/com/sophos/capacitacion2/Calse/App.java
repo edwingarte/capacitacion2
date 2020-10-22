6ab4e79f2ba22c24e45b1dc7383368feb509bd70
edwingarte <edwingarte@gmail.com>
2020-10-21 20:33:11 -0500

    clase1

---
+++ src/main/java/com/sophos/capacitacion2/Calse/App.java
@@ -0,0 +1,13 @@
+package com.sophos.capacitacion2.Calse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
